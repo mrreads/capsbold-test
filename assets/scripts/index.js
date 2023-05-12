b697550bce682b20860d62c0328943ee457616af
Alexander <mrreads@yandex.com>
2023-05-12 22:51:18 +0300

    final

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,18 +3,18 @@ let iterator = 0;
 
 function generateItem() {
     wrapper.innerHTML += `
-    <div class="todo__item">
+    <label for="todo_${iterator}" class="todo__item">
         <input id="todo_${iterator}" type="checkbox" class="todo__checkbox" />
-        <label for="todo_${iterator}" class="todo__label"> </label>
+        <div class="todo__label"> </div>
         
         <p class="todo__title">repellendus sunt dolores architecto voluptatum</p>
         <p class="todo__description">laboriosam mollitia et enim quasi adipisci quia provident illum</p>
-    </div>`;
+    </label>`;
 
     iterator++;
 }
 
-for(let i = 1; i <= 3; i++) {
+for(let i = 1; i <= 4; i++) {
     generateItem();
 }
 
